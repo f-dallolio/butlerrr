3a8585a6e7a2dee44540ac6d900c9ffaaea772c8
Filippo Dall'Olio <fdallolio@brocku.ca>
2024-09-14 23:13:08 -0400

    .

--- R/ns.R
+++ R/ns.R
@@ -3,18 +3,9 @@
 #' @param x A function, a string, or an environment.
 #' @param exports_only If `TRUE` (default), it returns only the exported functions. If `FALSE`, the private ones are included too.
 #'
-#' @return A named list of functions.
+#' @return A named list of functions or NULL if the environment of the function is not a namespace,
 #' @export
 #'
-#' @examples
-#' ns_get_fns(mean)
-#' ns_get_fns(environment(mean))
-#'
-#' export_fns <- ns_get_fns("base")
-#' all_fns <- ns_get_fns("base", exports_only = FALSE)
-#' length(export_fns)
-#' length(all)
-
 ns_get_fns <- function(x, exports_only = NULL){
   if(is.function(x)) x <- tryCatch(environment(x), error = function(e) asNamespace("base"))
   if(is.character(x)) x <- asNamespace(x)
